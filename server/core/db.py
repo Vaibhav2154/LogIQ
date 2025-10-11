7fe5ee43f103a74608f2aaf2355d640246bfd86b
Vaibhav2154 <vaibhavvaibhu2005@gmail.com>
2025-10-11 15:20:35 +0530

    added orchestrator

--- server/core/db.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from core.config import settings
-
-client = AsyncIOMotorClient(settings.MONGO_URL)
-database = client.Forensiq
-user_collection = database.get_collection("users")
-analysis_collection = database.get_collection("analysis_results")
-monitoring_collection = database.get_collection("monitoring_sessions")
